aa7eebdeb15d80a1aca09d1e26ff5e34af5a4970
Mun young Park <myi@memphis.edu>
2020-10-04 16:33:49 -0500

    Add more instrucion pages

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -3,11 +3,46 @@ class StaticPagesController < ApplicationController
     respond_to do |format|
       format.html { render :start }
   end
-end
+  end
 
 def instruction
   respond_to do |format|
     format.html { render :instruction }
   end
   end
+
+def instruction1
+  respond_to do |format|
+    format.html { render :instruction1 }
+    end
+    end
+def instruction1
+  respond_to do |format|
+    format.html { render :instruction1 }
+    end
+    end
+
+def instruction1
+    respond_to do |format|
+     format.html { render :instruction1 }
+      end
+      end
+
+def instruction1
+    respond_to do |format|
+      format.html { render :instruction1 }
+      end
+      end
+
+def instruction1
+    respond_to do |format|
+      format.html { render :instruction1 }
+      end
+       end
+
+def instruction1
+    respond_to do |format|
+      format.html { render :instruction1 }
+       end
+       end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,11 @@ Rails.application.routes.draw do
   get 'static_pages/start'
   get 'start', to: 'static_pages#start', as: 'start'
   get 'instruction', to: 'static_pages#instruction', as: 'instruction'
+  get 'instruction1', to: 'static_pages#instruction1', as: 'instruction1'
+  get 'instruction2', to: 'static_pages#instruction2', as: 'instruction2'
+  get 'instruction3', to: 'static_pages#instruction3', as: 'instruction3'
+  get 'instruction4', to: 'static_pages#instruction4', as: 'instruction4'
+  get 'instruction5', to: 'static_pages#instruction5', as: 'instruction5'
+  get 'instruction6', to: 'static_pages#instruction6', as: 'instruction6'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end

9610c21a652d10a66d284b4b74396d109c087327
Taylor <taylor@DESKTOP-ATHN8GG.localdomain>
2020-10-28 05:22:51 -0500

    fixed an error and added seeds

--- app/controllers/level_pages_controller.rb
+++ app/controllers/level_pages_controller.rb
@@ -2,9 +2,9 @@ class LevelPagesController < ApplicationController
 
   def level
     nodes = EasyLevel.all
+    nodess = IntermediateLevel.all
     respond_to do |format|
-      format.html { render :level, locals: { nodes: nodes} }
+      format.html { render :level, locals: { nodes: nodes, nodess: nodess} }
     end
   end
-
 end
